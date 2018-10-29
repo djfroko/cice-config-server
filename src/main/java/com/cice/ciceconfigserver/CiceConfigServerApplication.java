d1191eb872185b088755cc225f3d8cda52411cd7
cice <cice@A8POVMac12.local>
2018-10-29 20:20:20 +0100

    Initial commit

---
+++ src/main/java/com/cice/ciceconfigserver/CiceConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.cice.ciceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiceConfigServerApplication.class, args);
+	}
+}
